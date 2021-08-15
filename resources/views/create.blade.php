baaa72dc4b00738d1cb748c69dcd9597d896380f
Yuta Okuma <y.dream.nest@icloud.com>
2021-08-15 16:52:24 +0900

    ノート作成機能の実装（View側）

---
+++ resources/views/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">新規ノート作成</div>
+        <form class="card-body" action="/store" method="POST">
+            {{--なりすまし送信防止の対策として@csrfをつける（Cross Site Request Forgeries）--}}
+            {{--他人がなりすましてログインし、データを送信する攻撃手法。そのためLaravelのformではcsrfトークンを発行する--}}
+            @csrf
+            <div class="form-group">
+                <textarea class="form-control" name="content" rows="3" placeholder="ここに内容を入力"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">保存</button>
+        </form>
+    </div>
+@endsection

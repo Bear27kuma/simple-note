b0d8d74a0b1017943c4dc87e4bc54b589f56fb4e
Yuta Okuma <y.dream.nest@icloud.com>
2021-08-22 18:31:02 +0900

    タグ検索ロジックのリファクタリング

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
+
+    public function getMyNote() {
+        $query_tag = \Request::query('tag');
+        // ==== ベースのメソッド ====
+        $query = Note::select('notes.*')
+            ->where('user_id', '=', \Auth::id())
+            ->whereNull('deleted_at')
+            ->orderBy('updated_at', 'DESC');    // ASC=昇順、DESC=降順
+        // ==== ベースのメソッドここまで ====
+
+        // もしクエリパラメータにtagがあれば
+        if(!empty($query_tag)) {
+            // tagのidで絞り込み
+            // ここで、ノートを取得（whereNullで削除されていないものだけ取得）
+            $query->leftJoin('note_tags', 'note_tags.note_id', '=', 'notes.id')->where('note_tags.tag_id', '=', $query_tag);
+        }
+
+        $notes = $query->get();
+        return $notes;
+    }
 }

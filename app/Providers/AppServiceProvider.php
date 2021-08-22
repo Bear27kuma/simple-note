7d1ae3bab8b21cb7ff5d7c55f6f2ff82eab879e8
Yuta Okuma <y.dream.nest@icloud.com>
2021-08-22 18:13:09 +0900

    タグからの絞り込み検索（絞り込みロジック）

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,12 +27,26 @@ class AppServiceProvider extends ServiceProvider
     {
         // 全てのメソッドが呼ばれる前に先に呼ばれるメソッド
         view()->composer('*', function ($view) {
-            // ここで、ノートを取得（whereNullで削除されていないものだけ取得）
-            $notes = Note::select('notes.*')
-                ->where('user_id', '=', \Auth::id())
-                ->whereNull('deleted_at')
-                ->orderBy('updated_at', 'DESC')    // ASC=昇順、DESC=降順
-                ->get();
+            $query_tag = \Request::query('tag');
+            // もしクエリパラメータにtagがあれば
+            if(!empty($query_tag)) {
+                // tagのidで絞り込み
+                // ここで、ノートを取得（whereNullで削除されていないものだけ取得）
+                $notes = Note::select('notes.*')
+                    ->leftJoin('note_tags', 'note_tags.note_id', '=', 'notes.id')
+                    ->where('note_tags.tag_id', '=', $query_tag)
+                    ->where('user_id', '=', \Auth::id())
+                    ->whereNull('deleted_at')
+                    ->orderBy('updated_at', 'DESC')    // ASC=昇順、DESC=降順
+                    ->get();
+            } else {
+                // tagがなければ全て取得する
+                $notes = Note::select('notes.*')
+                    ->where('user_id', '=', \Auth::id())
+                    ->whereNull('deleted_at')
+                    ->orderBy('updated_at', 'DESC')
+                    ->get();
+            }
 
             $tags = Tag::where('user_id', '=', \Auth::id())
                 ->whereNull('deleted_at')
